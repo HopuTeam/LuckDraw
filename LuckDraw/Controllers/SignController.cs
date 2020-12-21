18ffd7bb480021f4a812157477db37e88490e7bd
Skilemon <35643936+Skilemon@users.noreply.github.com>
2020-12-21 23:40:31 +0800

    Fixed bugs

--- LuckDraw/Controllers/SignController.cs
+++ LuckDraw/Controllers/SignController.cs
@@ -25,21 +25,26 @@ namespace LuckDraw.Controllers
         [HttpPost]
         public IActionResult Sign(Sign sign)
         {
+            Sign mod;
             sign.Password = Security.MD5Encrypt32(sign.Password);
-            //var mod = EF.Signs.FirstOrDefault(x => x.Account == sign.Account && x.Password == sign.Password);
-            var mod = (from s in EF.Signs
-                       where s.Account == sign.Account && s.Password == sign.Password
-                       select s).FirstOrDefault();
-
-            if (mod != null)
+            if (sign.Account.Contains("@"))
             {
-                HttpContext.Session.SetModel("User", mod);
-                return Content("success");
+                mod = (from s in EF.Signs
+                       where s.Email == sign.Account && s.Password == sign.Password
+                       select s).FirstOrDefault();
             }
             else
             {
-                return Content("用户名或密码错误");
+                mod = (from s in EF.Signs
+                       where s.Account == sign.Account && s.Password == sign.Password
+                       select s).FirstOrDefault();
             }
+
+            if (mod == null)
+                return Content("用户名或密码错误");
+
+            HttpContext.Session.SetModel("User", mod);
+            return Content("success");
         }
 
         [HttpGet]
@@ -50,21 +55,21 @@ namespace LuckDraw.Controllers
         [HttpPost]
         public IActionResult Register(Sign sign)
         {
-            if (EF.Signs.FirstOrDefault(x => x.Account == sign.Account) == null)
-            {
-                sign.Status = false;
-                sign.Identity = 0;
-                sign.Password = Security.MD5Encrypt32(sign.Password);
-                EF.Signs.Add(sign);
-                if (EF.SaveChanges() > 0)
-                    return Content("success");
-                else
-                    return Content("表单数据异常");
-            }
-            else
-            {
+            if (EF.Signs.FirstOrDefault(x => x.Account == sign.Account) != null)
                 return Content("用户名被占用");
-            }
+
+            if (EF.Signs.FirstOrDefault(x => x.Email == sign.Email) != null)
+                return Content("邮箱已被绑定");
+
+            sign.Status = false;
+            sign.Identity = 0;
+            sign.Password = Security.MD5Encrypt32(sign.Password);
+            EF.Signs.Add(sign);
+
+            if (EF.SaveChanges() > 0)
+                return Content("success");
+            else
+                return Content("表单数据异常");
         }
 
         string code;
@@ -76,27 +81,19 @@ namespace LuckDraw.Controllers
         [HttpPost]
         public IActionResult Forget(string Code, Sign sign)
         {
-            if (Code == code)
-            {
-                var mod = EF.Signs.FirstOrDefault(x => x.Account == sign.Account && x.Email == sign.Email);
-                if (mod == null)
-                {
-                    return Content("邮箱或帐号错误");
-                }
-                else
-                {
-                    mod.Password = Security.MD5Encrypt32(sign.Password);
-
-                    if (EF.SaveChanges() > 0)
-                        return Content("success");
-                    else
-                        return Content("重置密码失败，请重试");
-                }
-            }
-            else
-            {
+            if (Code != code)
                 return Content("验证码错误");
-            }
+
+            var mod = EF.Signs.FirstOrDefault(x => x.Account == sign.Account && x.Email == sign.Email);
+            if (mod == null)
+                return Content("邮箱或帐号错误");
+
+            mod.Password = Security.MD5Encrypt32(sign.Password);
+
+            if (EF.SaveChanges() > 0)
+                return Content("success");
+            else
+                return Content("重置密码失败，请重试");
         }
 
         [HttpPost]
@@ -105,16 +102,12 @@ namespace LuckDraw.Controllers
             Random random = new Random();
             code = Security.MD5Encrypt16(random.Next(0, 9999).ToString()).Substring(random.Next(1, 9), 6).ToUpper();
             if (EF.Signs.FirstOrDefault(x => x.Account == Account && x.Email == mail) == null)
-            {
                 return Content("邮箱或帐号错误");
-            }
+
+            if (MailExt.SendMail(mail, "找回密码操作", $"尊敬的用户 { Account }：<br />您正在进行<span style='color:red;'>找回密码</span>操作！<br />本次操作的验证码是：<span style='color:red;'>{ code }</span>。<br />请注意谨防验证码泄露，保护账号安全！"))
+                return Content("success");
             else
-            {
-                if (MailExt.SendMail(mail, "找回密码操作", $"尊敬的用户 { Account }：<br />您正在进行<span style='color:red;'>找回密码</span>操作！<br />本次操作的验证码是：<span style='color:red;'>{ code }</span>。<br />请注意谨防验证码泄露，保护账号安全！"))
-                    return Content("success");
-                else
-                    return Content("邮件发送失败");
-            }
+                return Content("邮件发送失败");
         }
     }
 }
--- LuckDraw/Controllers/UserController.cs
+++ LuckDraw/Controllers/UserController.cs
@@ -22,6 +22,12 @@ namespace LuckDraw.Controllers
             return View(EF.Signs.FirstOrDefault(x => x.ID == HttpContext.Session.GetModel<Sign>("User").ID));
         }
 
+        //[HttpPost]
+        //public IActionResult Edit(Sign sign)
+        //{
+
+        //}
+
         [HttpPost]
         public IActionResult Logout()
         {
@@ -33,26 +39,18 @@ namespace LuckDraw.Controllers
         public IActionResult Auth(string Code, Sign sign)
         {
             if (Code == HttpContext.Session.GetString("Code"))
-            {
-                var mod = EF.Signs.FirstOrDefault(x => x.Account == sign.Account && x.Email == sign.Email);
-                if (mod == null)
-                {
-                    return Content("<script>alert('account or mail info error');window.location.href='/User/Index';</script>", "text/html", System.Text.Encoding.UTF8);
-                }
-                else
-                {
-                    mod.Status = true;
-
-                    if (EF.SaveChanges() > 0)
-                        return Content("<script>alert('认证成功');window.location.href='/User/Index';</script>", "text/html", System.Text.Encoding.UTF8);
-                    else
-                        return Content("<script>alert('认证失败，请重试');window.location.href='/User/Index';</script>", "text/html", System.Text.Encoding.UTF8);
-                }
-            }
-            else
-            {
                 return Content("<script>alert('认证码错误');window.location.href='/User/Index';</script>", "text/html", System.Text.Encoding.UTF8);
-            }
+
+            var mod = EF.Signs.FirstOrDefault(x => x.Account == sign.Account && x.Email == sign.Email);
+            if (mod == null)
+                return Content("<script>alert('认证信息异常');window.location.href='/User/Index';</script>", "text/html", System.Text.Encoding.UTF8);
+
+            mod.Status = true;
+
+            if (EF.SaveChanges() > 0)
+                return Content("<script>alert('认证成功');window.location.href='/User/Index';</script>", "text/html", System.Text.Encoding.UTF8);
+            else
+                return Content("<script>alert('认证失败，请重试');window.location.href='/User/Index';</script>", "text/html", System.Text.Encoding.UTF8);
         }
 
         [HttpPost]
@@ -60,25 +58,17 @@ namespace LuckDraw.Controllers
         {
             var mod = HttpContext.Session.GetModel<Sign>("User");
             if (mod == null)
-            {
                 return Redirect("/Sign/Index");
-            }
+
+            Random random = new Random();
+            HttpContext.Session.SetString("Code", Security.MD5Encrypt16(random.Next(0, 9999).ToString()).Substring(random.Next(1, 16), 6).ToUpper());
+            if (EF.Signs.FirstOrDefault(x => x.Email == mod.Email) == null)
+                return Content("邮箱验证错误");
+
+            if (MailExt.SendMail(mod.Email, "账户验证操作", $"尊敬的用户 { mod.Account }：<br />您正在进行<span style='color:skyblue;'>账户认证</span>操作！<br />请点击[<a href='https://localhost:44318/User/Auth?Code={ HttpContext.Session.GetString("Code") }&Account={ mod.Account }&Email={ mod.Email }'>本链接</a>]进行认证。<br />请注意谨防验证码泄露，保护账号安全！"))
+                return Content("success");
             else
-            {
-                Random random = new Random();
-                HttpContext.Session.SetString("Code", Security.MD5Encrypt16(random.Next(0, 9999).ToString()).Substring(random.Next(1, 9), 6).ToUpper());
-                if (EF.Signs.FirstOrDefault(x => x.Email == mod.Email) == null)
-                {
-                    return Content("邮箱错误");
-                }
-                else
-                {
-                    if (MailExt.SendMail(mod.Email, "账户验证操作", $"尊敬的用户 { mod.Account }：<br />您正在进行<span style='color:skyblue;'>账户认证</span>操作！<br />请点击[<a href='https://localhost:44318/User/Auth?Code={ HttpContext.Session.GetString("Code") }&Account={ mod.Account }&Email={ mod.Email }'>本链接</a>]进行认证。<br />请注意谨防验证码泄露，保护账号安全！"))
-                        return Content("success");
-                    else
-                        return Content("邮件发送失败");
-                }
-            }
+                return Content("邮件发送失败");
         }
     }
 }
