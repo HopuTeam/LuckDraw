a56f4d66052ea60011d0aab367a9275eb02f7c46
Skilemon <35643936+Skilemon@users.noreply.github.com>
2020-12-21 13:02:07 +0800

    Fixed Bugs

--- LuckDraw/Controllers/UserController.cs
+++ LuckDraw/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LuckDraw.Handles;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,13 @@ namespace LuckDraw.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.SetModel("User", null);
+            return Content("success");
         }
     }
 }
